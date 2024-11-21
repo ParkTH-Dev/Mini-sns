5904d2d184bd822a254c77607a0d14fbacedefaa
ParkTH-Dev <xotj2635@naver.com>
2024-11-21 17:12:50 +0900

    update

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -161,8 +161,15 @@ const ProfileImage = styled.img`
   width: 40px;
   height: 40px;
   border-radius: 50%;
-  object-fit: cover;
-  border: 1px solid ${(props) => props.theme.colors.border};
+`;
+
+const DefaultAvatar = styled.svg`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.colors.primary};
+  padding: 5px;
+  color: white;
 `;
 
 const Post = ({
@@ -268,10 +275,25 @@ const Post = ({
       <Item>
         <div className="content">
           <UserProfile>
-            <ProfileImage
-              src={profileUrl || "/default-profile.png"}
-              alt={`${username}'s profile`}
-            />
+            {profileUrl ? (
+              <ProfileImage src={profileUrl} />
+            ) : (
+              <DefaultAvatar
+                data-slot="icon"
+                fill="none"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+              </DefaultAvatar>
+            )}
             <UserName>{username}</UserName>
           </UserProfile>
           {photo ? <PostImg src={photo} alt="Post content" /> : null}
